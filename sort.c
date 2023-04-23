e266400e3486f34da47d519c8014764d55c753f9
Ryan McFall <mcfall@hope.edu>
2023-04-23 15:00:16 -0400

    Version with size = 1024

---
+++ sort.c
@@ -0,0 +1,23 @@
+#define SIZE 1024
+#include <stdlib.h>
+#include <time.h>
+
+int values[SIZE];
+
+int cmp(const void* a, const void* b) {
+    const int* A = a;
+    const int* B = b;
+
+    return (*A > *B) - (*A < *B);
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+
+    for (int i = 0; i < SIZE; i++) {
+        values[i] = rand();
+    }
+
+    qsort(values, SIZE, sizeof(int), cmp);
+}
